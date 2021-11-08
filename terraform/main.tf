277f5ac92429f81b0de13efe6f6931bd9d45536e
chow jing lun <64826311+chowjl94@users.noreply.github.com>
2021-11-08 17:14:54 +0800

    update main.tf d4

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,99 @@
-resource "aws_s3_bucket" "state_backend" {
-  # Bucket names must be unique across all the people & organizations using AWS
-  bucket = "terraform-state-bucket-state-backend"
-  acl    = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
+##d4
+data "aws_vpc" "default_vpc" {
+  default = true
 }
 
-resource "aws_s3_bucket_public_access_block" "default" {
-  bucket                  = aws_s3_bucket.state_backend.id
-  block_public_acls       = true
-  ignore_public_acls      = true
-  block_public_policy     = true
-  restrict_public_buckets = true
+data "aws_ami" "docker_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["*amazon-ecs-optimized*"]
+  }
+}
+
+variable "ec2_key" {
+  type = string
+}
+resource "tls_private_key" "this" {
+  algorithm = "RSA"
+}
+
+resource "aws_key_pair" "ec2_key" {
+  key_name   = "ec2_key"       
+  public_key = "${var.ec2_key}"
+}
+
+output "key_name" {
+  description = "SSH Private Key Name"
+  value       = aws_key_pair.ec2_key
+}
+resource "aws_security_group" "pub" {
+  name        = "app-firewall"
+  description = "Rules for public outbound"
+  vpc_id      = data.aws_vpc.default_vpc.id
+}
+
+resource "aws_security_group_rule" "public_outbound" {
+  type              = "egress"
+
+  ### all ip addresses
+  from_port         = 0
+  to_port           = 0
+
+  ### all protocols
+  protocol          = "-1"
+
+  ### all ipv4 address
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.pub.id
+}
+
+
+
+
+resource "aws_security_group_rule" "pub_inbound_ssh" {
+  type              = "ingress"
+
+  ### ssh port is 22
+  from_port         = 22
+  to_port           = 22
+
+  ### ssh works on tcp
+  protocol          = "tcp"
+
+  ### allow all ssh traffic from any location
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.pub.id
+}
+
+resource "aws_security_group_rule" "pub_inbound_http_rule" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.pub.id
+}
+
+resource "aws_security_group_rule" "public_inbound_https_rule" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.pub.id
+}
+
+
+
+resource "aws_instance" "app_server" {
+  ami = data.aws_ami.docker_ami.id
+  instance_type = "t2.micro"
+  key_name = "ec2_key"
+  vpc_security_group_ids = [aws_security_group.pub.id]
+  tags = {
+      Name = "amongustodo-api"
+  }
 }
